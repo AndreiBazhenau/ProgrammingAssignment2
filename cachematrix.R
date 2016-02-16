420196efc5f811eab4b03c17773dc37def6d9f20
AndreiBazhenau <ab@ikiss.biz>
2016-02-16 08:59:45 +0200

    Programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Make vache matrix object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    if (!is.matrix(x)) {
+        warning("x must be matrix!")
+        return(NaN)
+    }
+    
+    inversematrix <- NULL
+    set <- function(y) {
+        x <<- y
+        inversematrix <<- NULL
+    }
+    get <- function() x
+    
+    set_inversematrix <- function(new_inversematrix) { 
+        inversematrix <<- new_inversematrix
+    }
+    get_inversematrix <- function() inversematrix
+    
+    list(set = set, get = get,
+         set_inversematrix = set_inversematrix,
+         get_inversematrix = get_inversematrix)
 }
 
 
-## Write a short comment describing this function
+## Get inverse matrix from cache or calculate it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    if (is.null(x) || is.na(x)) {
+        warning("x is undefined!")
+        return(NaN)
+    }
+    
+    data <- x$get()
+    if (nrow(data) != ncol(data)) {
+        warning("Matrix is not invertable: (num rows != num cols) !")
+        return(NaN)
+    }
+    
+    if (det(data) == 0) {
+        warning("Matrix is not invertable: (determinant == 0)!")
+        return(NaN)        
+    }
+    
+    inversematrix <- x$get_inversematrix()
+    if(!is.null(inversematrix)) {
+        message("getting cached data")
+        return(inversematrix)
+    }
+    
+    inversematrix <- solve(data, ...)
+    x$set_inversematrix(inversematrix)
+    
+    inversematrix
 }
